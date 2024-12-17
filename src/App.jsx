103d6ae804ce5339266694dca9848af9f81feffb
SINGH <anuragsing10@deloitte.com>
2024-12-17 12:27:00 +0530

    Parallax+Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Carousel from './HomePage/Carousal';
 import Login from './LoginPage/Login';
+import Parallaxx from './HomePage/parallaxx';
+import FancyButton from './fancyButton';
 
 function App() {
     return (
+    <>
+    <Login />
+    <Parallaxx/>
+    </>
         // <Carousel />
-        <Login />
+        
     );
 }
 
---
+++ src/FancyButton.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useRef } from "react";
+import styles from './FancyButton.module.css'
+const FancyButton = () => {
+  
+
+    return (
+     
+      <div>
+      <button className={styles.button}>MORE DETAILS</button>
+      </div>
+    );
+  };
+  
+  export default FancyButton;
