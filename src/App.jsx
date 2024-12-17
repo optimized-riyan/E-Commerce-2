287643607595c96b2fb78dab02aec4fbd2cf3217
Riyan Gonsalves <114173214+optimized-riyan@users.noreply.github.com>
2024-12-17 15:39:50 +0530

    carousal: fix image dimension

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     return (
         <>
             <Login />
-            {/* <Carousel /> */}
+            <Carousel />
             <Parallaxx/>
         </>
     );
--- src/HomePage/parallaxx.jsx
+++ src/HomePage/parallaxx.jsx
@@ -9,11 +9,11 @@ const Parallaxx = () => {
       <ParallaxProvider>
       <div className={styles.container} >
         <Parallax speed={20} style={{display:'flex', flexDirection:'row', justifyContent:"space-between", alignItems:'center', marginTop:'1em', alignContent:'center', padding:'10px'}}>
-        <img src="https://media.istockphoto.com/id/1136478981/vector/cute-woodland-forest-animal-vector-illustration-set.jpg?s=1024x1024&w=is&k=20&c=FTNHgz7TZOyOOe4YVPQHUVqHnx82-FReg_UEidbjXHY=" alt="" />
+        <img className={styles.img} src="https://media.istockphoto.com/id/1136478981/vector/cute-woodland-forest-animal-vector-illustration-set.jpg?s=1024x1024&w=is&k=20&c=FTNHgz7TZOyOOe4YVPQHUVqHnx82-FReg_UEidbjXHY=" alt="" />
         <Parallax  speed={-10} style={{display:'flex', flexDirection:'column', justifyContent:"space-between",flex:1, alignItems:'center'}}>
       <div className={styles.text}> Vitra's Environmental Mission</div> <br /><FancyButton text={'LEARN MORE'} width="fit-content"/>
       </Parallax>
-        <img src="https://media.istockphoto.com/id/1136478981/vector/cute-woodland-forest-animal-vector-illustration-set.jpg?s=1024x1024&w=is&k=20&c=FTNHgz7TZOyOOe4YVPQHUVqHnx82-FReg_UEidbjXHY=" alt="" />
+        <img className={styles.img} src="https://media.istockphoto.com/id/1136478981/vector/cute-woodland-forest-animal-vector-illustration-set.jpg?s=1024x1024&w=is&k=20&c=FTNHgz7TZOyOOe4YVPQHUVqHnx82-FReg_UEidbjXHY=" alt="" />
        </Parallax>
       </div>
       </ParallaxProvider>
