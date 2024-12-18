2b3338d1e1e58af6d96e6362da6423f4e3ed3e37
Riyan Gonsalves <114173214+optimized-riyan@users.noreply.github.com>
2024-12-18 22:01:30 +0530

    home page put together

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
-import Carousel from './HomePage/Carousal';
-import Login from './LoginPage/Login';
-import Parallaxx from './HomePage/parallaxx';
-import FancyButton from './FancyButton';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import UserData from './HomePage/userdata';
-import Form from './LoginPage/Form';
+import Login from './LoginPage/Login';
+import Home from './HomePage/Home'
+
 function App() {
     return (
-        <>
-           
-           
-
-            <Router>
-           <Parallaxx/>
-            <Carousel />
-                  <Routes>
-                    <Route path="/" element={<Form />} />
-                    <Route path="/userdata" element={<UserData />} />
-                  </Routes>
-                </Router>
-        </>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </Router>
     );
 }
 
