19bae1efce0edb7dc1aebc0d5608ef42012d796e
Chhabria <achhabri@deloitte.com>
2024-12-18 18:19:34 +0530

    merese nahi hoga

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,21 @@ import FancyButton from './FancyButton';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserData from './HomePage/userdata';
 import Form from './LoginPage/Form';
+import ProductPage from './ProductPage/ProductPage';
+
 function App() {
     return (
         <>
            
            
-
+          
             <Router>
            <Parallaxx/>
             <Carousel />
                   <Routes>
                     <Route path="/" element={<Form />} />
                     <Route path="/userdata" element={<UserData />} />
+                    <Route path="/products" element={<ProductPage />} />
                   </Routes>
                 </Router>
         </>
