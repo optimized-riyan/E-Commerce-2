6962137f2c0798e98b85060e701ba337f7234063
Saishree Pattnaik <saishreeajk@gmail.com>
2024-12-18 09:33:38 +0530

    new changes in nav

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import Carousel from './HomePage/Carousal';
 import Login from './LoginPage/Login';
 import Parallaxx from './HomePage/parallaxx';
 import FancyButton from './fancyButton';
+import NavBar from './NavBar/NavBar'
 
 function App() {
     return (
     <>
+    
     <Login />
+    <NavBar/> 
     <Parallaxx/>
     </>
         // <Carousel />
