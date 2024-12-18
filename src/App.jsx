f8f061773ee29ca41382428e2c98e85b848a2f20
SINGH <anuragsing10@deloitte.com>
2024-12-18 11:25:10 +0530

    [Email-Password Validation] form, formcontrol, userdata, fancy button.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import Carousel from './HomePage/Carousal';
 import Login from './LoginPage/Login';
 import Parallaxx from './HomePage/parallaxx';
 import FancyButton from './FancyButton';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserData from './HomePage/userdata';
+import Form from './LoginPage/Form';
 function App() {
     return (
         <>
-            <Login />
+            {/* <Login />
             {/* <Carousel /> */}
-            <Parallaxx/>
+           
+
+            <Router>
+                  <Routes>
+                    <Route path="/" element={<Form />} />
+                    <Route path="/userdata" element={<UserData />} />
+                  </Routes>
+                </Router>
         </>
     );
 }
--- src/LoginPage/FormControl.jsx
+++ src/LoginPage/FormControl.jsx
@@ -1,12 +1,12 @@
 import styles from './FormControl.module.css';
 
-export default function FormControl({label = 'No Label', type = 'text', rootStyleOverride = {}}) {
+export default function FormControl({label = 'No Label', type = 'text', rootStyleOverride = {}, onChange}) {
     let inputId = crypto.randomUUID();
     return (
         <div className={styles.outerContainer} style={rootStyleOverride}>
             <div className={styles.innerContainer}>
                 <label htmlFor={inputId} className={styles.label}>{label}</label><br />
-                <input className={styles.input} type="text" id={inputId} />
+                <input className={styles.input} type="text" id={inputId}  onChange={onChange}/>
             </div>
         </div>
     );
