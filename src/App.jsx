55913c8a148e6a4be5951d20c90f629ade5bb235
Saishree Pattnaik <saishreeajk@gmail.com>
2024-12-19 13:22:27 +0530

    about us done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import Carousel from './HomePage/Carousal';
-import Login from './LoginPage/Login';
-import Parallaxx from './HomePage/parallaxx';
-import FancyButton from './FancyButton';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import UserData from './HomePage/userdata';
-import Form from './LoginPage/Form';
+// import Carousel from './HomePage/Carousal';
+// import Login from './LoginPage/Login';
+// import Parallaxx from './HomePage/parallaxx';
+// import FancyButton from './FancyButton';
+// // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import UserData from './HomePage/userdata';
+// import Form from './LoginPage/Form';
+import Footer from './Footer/Footer';
+import NavBar from './NavBar/NavBar';
+import AboutUs from './About/aboutUs';
+import CoverImage from './About/CoverImage'
 function App() {
     return (
         <>
            
            
-
-            <Router>
+          
+            {/* <Router>
+            
            <Parallaxx/>
             <Carousel />
                   <Routes>
                     <Route path="/" element={<Form />} />
                     <Route path="/userdata" element={<UserData />} />
                   </Routes>
-                </Router>
+                </Router> */}
+                <NavBar/>
+              <CoverImage/>
+              <AboutUs/> 
+               <Footer/> 
         </>
     );
 }
