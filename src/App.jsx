f9209f1078b7687115f1893ed570dd07bb6e9ff1
Riyan Gonsalves <114173214+optimized-riyan@users.noreply.github.com>
2024-12-19 12:25:04 +0530

    integrate cart into routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from './LoginPage/Login';
 import Home from './HomePage/Home';
 import ProductPage from './ProductPage/ProductPage';
 import UserData from './HomePage/userdata';
+import CartDetails from "./Cart/Cart";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/products" element={<ProductPage />} />
                 <Route path="/userdata" element={<UserData />} />
+                <Route path="/cart" element={<CartDetails />}/>
             </Routes>
         </Router>
     );
