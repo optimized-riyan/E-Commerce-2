7a9b1a1ee3c407a37fb2333cdf68d216ad892190
Pahlani <ypahlani@deloitte.com>
2024-12-19 14:38:45 +0530

    product-details-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import Carousel from './HomePage/Carousal';
-import Login from './LoginPage/Login';
-import Parallaxx from './HomePage/parallaxx';
-import FancyButton from './FancyButton';
+import Carousel from "./HomePage/Carousal";
+import Login from "./LoginPage/Login";
+import Parallaxx from "./HomePage/parallaxx";
+import FancyButton from "./FancyButton";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import UserData from './HomePage/userdata';
-import Form from './LoginPage/Form';
-import ProductPage from './ProductPage/ProductPage';
+import UserData from "./HomePage/userdata";
+import Form from "./LoginPage/Form";
+import ProductPage from "./ProductPage/ProductPage";
+import ProductDetail from "./ProductDetail/ProductDetail";
 
 function App() {
-    return (
-        <>
-           
-           
-          
-            <Router>
-           <Parallaxx/>
-            <Carousel />
-                  <Routes>
-                    <Route path="/" element={<Form />} />
-                    <Route path="/userdata" element={<UserData />} />
-                    <Route path="/products" element={<ProductPage />} />
-                  </Routes>
-                </Router>
-        </>
-    );
+  return (
+    <>
+      <Router>
+        {/* <Parallaxx />
+        <Carousel /> */}
+        <Routes>
+          <Route path="/" element={<Form />} />
+          <Route path="/userdata" element={<UserData />} />
+          <Route path="/products" element={<ProductPage />} />
+          <Route path="/products/detail/:id" element={<ProductDetail />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
