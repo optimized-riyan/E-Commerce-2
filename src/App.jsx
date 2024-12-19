6a849fb998fd03d3d22664fce558ea82aa0fae9c
sinferno-1 <narurtosingh@gmail.com>
2024-12-19 12:07:07 +0530

    Cart Page added, [pass id via props]

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './LoginPage/Login';
-import Home from './HomePage/Home'
+import Home from './HomePage/Home';
+import ProductPage from './ProductPage/ProductPage';
+import UserData from './HomePage/userdata';
 
 function App() {
     return (
@@ -8,6 +10,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/products" element={<ProductPage />} />
+                <Route path="/userdata" element={<UserData />} />
             </Routes>
         </Router>
     );
--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -7,22 +7,20 @@ import Carousal from './Carousal';
 import Footer from '../Footer/Footer';
 import Form from '../LoginPage/Form';
 import { Route, Router, Routes } from 'react-router-dom';
-
+import UserData from './userdata';
+// import Cart from '../Cart/Cart';
+import CartDetails from '../Cart/Cart';
 export default function Home() {
     return (
-        <>
-            <Navbar />
+      <>
+        <Navbar />
             <Video />
             <Catalogue />
             <Parallaxx />
             <AboutVitra />
             <Carousal margin='1rem 0 6rem 0' />
             <Footer />
-           
-                
-                <Form />
-             
-
-        </>
+        <CartDetails />
+                </>
     );
 }
