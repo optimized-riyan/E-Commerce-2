11a9518ca00f0cbb67c611e43dcb39c05011cc3b
Riyan Gonsalves <114173214+optimized-riyan@users.noreply.github.com>
2024-12-20 12:07:22 +0530

    cart fetch using user id

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -1,17 +1,24 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styles from './CartDetails.module.css';
 import { FaPaypal } from "react-icons/fa";
 import { FaGooglePay } from "react-icons/fa";
 import { SiPhonepe } from "react-icons/si";
 import { SiPaytm } from "react-icons/si";
+import UserContext from '../UserContext';
+import { useNavigate } from 'react-router-dom';
 
 const CartDetails = () => {
-  const [cartId, setCartId] = useState('');
+  const {user: {id: cartId}} = useContext(UserContext);
   const [cart, setCart] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getCartDetails();
+  }, []);
 
   const getCartDetails = async () => {
     if (!cartId) {
-      alert('Please enter a cart ID');
+			navigate('/login');
       return;
     }
 
@@ -40,13 +47,13 @@ const CartDetails = () => {
   return (
     <div style={{ padding: '20px' }}>
       <h1>Cart Details</h1>
-      <input
+      {/* <input
         type="number"
         value={cartId}
         onChange={(e) => setCartId(e.target.value)}
         placeholder="Enter Cart ID"
       />
-      <button onClick={getCartDetails}>Get Cart Details</button>
+      <button onClick={getCartDetails}>Get Cart Details</button> */}
 
       {cart && (
         <div className="cart-details" style={{ marginTop: '20px' }}>
