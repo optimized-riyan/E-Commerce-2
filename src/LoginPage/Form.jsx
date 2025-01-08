4f3878c1fd9bfb9334697f2d2df63fa0cdca6845
Riyan Gonsalves <114173214+optimized-riyan@users.noreply.github.com>
2025-01-08 10:54:42 +0530

    feat(login): update fetch to use app's db
    - shorted db.json from excess users

--- src/LoginPage/Form.jsx
+++ src/LoginPage/Form.jsx
@@ -11,13 +11,14 @@ export default function Form() {
   const [userData, setUserData] = useState([]);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
   const { setUser } = useContext(UserContext);
 
   const getAPIData = async () => {
     try {
-      const response = await axios.get("https://dummyjson.com/users");
-      setUserData(response.data.users);
+      const response = await axios.get("http://localhost:3000/users");
+      setUserData(response.data);
     } catch (error) {
       console.log("Error1");
     }
@@ -26,13 +27,8 @@ export default function Form() {
   useEffect(() => {
     getAPIData();
   }, []);
-  useEffect(() => {
-    console.log("Updated", userData);
-  }, [userData]);
 
   const handleLogin = () => {
-    console.log("Email", email);
-    console.log("Password", password);
     const user = userData.find(
       (user) => user.email === email && user.password === password
     );
@@ -42,7 +38,7 @@ export default function Form() {
       navigate("/");
       setUser(user);
     } else {
-      console.log("Invalid email or password");
+      setError("Invalid email or password");
     }
   };
 
@@ -51,6 +47,7 @@ export default function Form() {
             <h1 className={styles.title}>Login</h1>
             <FormControl label="Email" type="email" rootStyleOverride={{ margin: '1rem 0' }}  onChange={(e) => setEmail(e.target.value)}/>
             <FormControl label="Password" type="password" onChange={(e) => setPassword(e.target.value)}/>
+            <pre style={{ color: 'red', whiteSpace: 'pre-line' }}>{error}</pre>
             <FancyButton text="LOGIN" width="230px" margin="3rem auto .5rem auto" onClick ={()=>handleLogin()}/>
             <FancyButton text="NEW HERE?" width="230px" margin=".5rem auto 3rem auto" onClick={() => navigate('/signup')} />
         </div>
